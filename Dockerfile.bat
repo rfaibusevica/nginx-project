d8a611fc89a2627c49a9ac983f7ccc45aa936ae0
RamonaFaib <74114728+RamonaFaib@users.noreply.github.com>
2021-02-19 17:59:15 +0200

    Update Dockerfile.bat

--- Dockerfile.bat
+++ Dockerfile.bat
@@ -1,7 +1,7 @@
 FROM httpd:2.4
 
-COPY index.html /usr/local/apache2/htdocs/
-COPY card.png /usr/local/apache2/htdocs/
+COPY ./index.html /usr/local/apache2/htdocs/
+COPY ./card.png /usr/local/apache2/htdocs/
 
 EXPOSE 80
 
